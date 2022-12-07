caf64a7bee7fda0eb42229c6cbdf8e93b32baed2
Praba <prama1303@gmail.com>
2022-12-07 23:03:21 +0100

    upload a single file setup

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import multer from 'multer';
 
 const app = express();
-
 dotenv.config();
+
+//Sigle file upload
+const upload = multer({ dest: 'uploads/' });
+//middleware key name is file
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.json({ status: 'Success' });
+});
 
 const PORT = process.env.PORT || 6000;
 
